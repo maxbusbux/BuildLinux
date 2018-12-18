649ed2d27ff2de49f9158de73723bf32f2a9a969
wf <aas@111.ru>
2018-12-18 15:41:18 +0300

    FSFSFI

--- buildLinux_ubuntu16.04.bash
+++ buildLinux_ubuntu16.04.bash
@@ -1,35 +1,24 @@
 toilet -F gay  "BuildLinux"
 wait
 sleep 3
-echo "installing git"
-sleep 3
-sudo apt install git
 wait
 sleep 1
 echo dowloading linux-kernel
 sleep 5
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.15.7.tar.xz
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.10.tar.xz
 wait 
 echo unpacking archive
 sleep 1
-tar xf linux-4.15.7.tar.xz
+tar xvfJ  linux-4.19.0.tar.xz
 sleep 2
 echo installing requirements
 sleep 4
-sudo apt-get installl build-dep linux
-wait
-sudo apt install kernel-package
-wait
-sudo apt install fakeroot
-wait
-sudo apt install libncursesw5-dev
-wait
-sudo apt install libssl-dev
+sudo apt-get installl build-dep-linux  kernel-package fakerootsudo  libncursesw5-dev libssl-dev
 wait
 sleep 1
 echo going into kernel directory
 sleep 1
-cd linux-4.15.7
+cd linux-4.19.0
 sleep 1
 echo making menuconfig
 sleep 2
@@ -38,6 +27,4 @@ wait
 sleep 2
 sudo fakeroot make-kpkg -j 5 --initrd --append-to-version=-custom kernel_image kernel_headers #-j
 wait
-sleep 2
-toilet -F metal -F gay "Thanks For Using this software!"
 sleep 2
